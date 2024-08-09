ccb9d94999e3e61ba9fc42d78ca395b59d70dde6
Julian-Panditharatne <julianpanditharatne@hotmail.com>
2024-08-09 14:02:01 -0400

    Started getting the data from the JSON and creating functions to help create the final visual

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,3 +1,13 @@
 // Store the earthquake JSON URL
 let earthquakeJSON = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
+// Get the JSON data from that URL
+d3.json(earthquakeJSON).then((data) => {
+    // Get the features field, which contains the earthquake data
+    let quakeData = data.features;
+
+    // Function for creating the circle markers for each feature
+    function pointToLayer(geoJsonPoint, latlng) {
+
+    };
+});
